f2214fc997d754aafcb6662e5571a0bf33ff113f
a5720080 <a5720080@gmail.com>
2017-05-28 16:33:00 +0700

    Use render template shortcut

--- rtv/views.py
+++ rtv/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
 from .models import Report
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     all_report = Report.objects.all()
-    template = loader.get_template('rtv/index.html')
     context = {
         'all_report' : all_report,
     }
-    return HttpResponse(template.render(context , request))
+    return render(request , 'rtv/index.html',context)
 
 def detail(request,report_id):
     return HttpResponse("<h2>Detail for Rreport id " + str(report_id) + "</h2>")
