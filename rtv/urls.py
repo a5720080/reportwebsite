b15ab1ad3d7168699b12959c8b21cde0553acb22
a5720080 <a5720080@gmail.com>
2017-05-28 16:01:04 +0700

    Create detail in view.py urls.py

--- rtv/urls.py
+++ rtv/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #/rtv/
     url(r'^$',views.index , name ='index'),
+    #/rtv/123/
+    url(r'^(?P<report_id>[0-9]+)$',views.detail , name = 'detail'),
 ]
--- rtv/views.py
+++ rtv/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is Rtv app homepage </h1>")
+
+def detail(request,report_id):
+    return HttpResponse("<h2>Detail for Rreport id " + str(report_id) + "</h2>")
