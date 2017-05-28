b806771bce03dd35c4c3818793b886d9ad795645
a5720080 <a5720080@gmail.com>
2017-05-28 16:23:10 +0700

    Use template for index

--- rtv/views.py
+++ rtv/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from .models import Report
+from django.template import loader
 
 def index(request):
-    return HttpResponse("<h1>This is Rtv app homepage </h1>")
+    all_report = Report.objects.all()
+    template = loader.get_template('rtv/index.html')
+    context = {
+        'all_report' : all_report,
+    }
+    return HttpResponse(template.render(context , request))
 
 def detail(request,report_id):
     return HttpResponse("<h2>Detail for Rreport id " + str(report_id) + "</h2>")
