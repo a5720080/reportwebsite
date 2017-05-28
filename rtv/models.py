f7dd45a2260198580d856ff3b694d64c65fdf289
a5720080 <a5720080@gmail.com>
2017-05-28 15:32:46 +0700

    Create models

--- rtv/models.py
+++ rtv/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    topic = models.CharField(max_length = 100)
+    license_plate = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 100)
+    date = models.DateTimeField(auto_now = True , blank = False)
+
+class order(models.Model):
+    report = models.ForeignKey(Report, on_delete = models.CASCADE)
