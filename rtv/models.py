0348ab690015503de48d2a7b7a6d5fd21deff05c
a5720080 <a5720080@gmail.com>
2017-05-28 15:49:35 +0700

    Edit model and create admin mode

--- rtv/models.py
+++ rtv/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 class Report(models.Model):
     topic = models.CharField(max_length = 100)
-    license_plate = models.CharField(max_length = 10)
+    license_plate = models.CharField(max_length = 30)
     address = models.CharField(max_length = 100)
-    date = models.DateTimeField(auto_now = True , blank = False)
+    date = models.DateTimeField(blank = False)
+
+
+    def __str__(self):
+        return self.topic + ' - ' + self.license_plate
 
 class order(models.Model):
     report = models.ForeignKey(Report, on_delete = models.CASCADE)
