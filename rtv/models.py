7160ac2cdafcde90b15d183056413d8f2fddca90
a5720080 <a5720080@gmail.com>
2017-05-29 19:19:23 +0700

    Debug add post

--- rtv/models.py
+++ rtv/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Report(models.Model):
     topic = models.CharField(max_length = 100)
     license_plate = models.CharField(max_length = 30)
     address = models.CharField(max_length = 100)
     date = models.DateTimeField(blank = False)
+    image = models.FileField()
 
+
+    def get_absolute_url(self):
+        return reverse('rtv:detail', kwargs = {'pk':self.pk})
 
     def __str__(self):
         return self.topic + ' - ' + self.license_plate
